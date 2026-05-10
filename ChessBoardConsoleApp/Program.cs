6e4ec5a828347184ca3b2eab29236e9799c8cdfe
Patrick <pbrewskiii@gmail.com>
2026-05-10 08:53:33 -0400

    Add user input from chess piece to console app

--- ChessBoardConsoleApp/Program.cs
+++ ChessBoardConsoleApp/Program.cs
@@ -5,15 +5,30 @@
  * Chess Board Project
  * Activity 2
  */
+
+using ChessBoardClassLibrary.Models;
 //--------------------------------
 // Start of Main Method
 //--------------------------------
-
+ 
+//Declare and initialize
+string piece = "";
+Tuple<int, int>? result;
 // Print a welcome message for the user
-
-using ChessBoardClassLibrary.Models;
-
 Console.WriteLine("Hello, Chess Players!");
+
+// Create a new chess board
+BoardModel board = new BoardModel(8);
+// Show the empty board
+Utility.PrintBoard(board);
+// Prompt the user for the type of chess piece
+Console.Write("Enter the type of piece you want to place (Knight, Rook, Bishop, Queen, King): ");
+piece = Console.ReadLine();
+// Prompt the user for the location of the chess piece
+result = Utility.GetRowAndCol();
+// Mark the legal moves based on the input
+
+// Print out the new chess board
 
 //--------------------------------
 // End of Main Method
@@ -57,4 +72,18 @@ public static class Utility
             Console.WriteLine();
         }
     } //End of PrintBoard method
+
+    internal static Tuple<int, int> GetRowAndCol()
+    {
+        //Get the row from the user
+        Console.Write("Enter the row number of the piece: ");
+        int row = int.Parse(Console.ReadLine());
+        
+        //Get the column from the user
+        Console.Write("Enter the column number of the piece: ");
+        int col = int.Parse(Console.ReadLine());
+        
+        //Return the data
+        return Tuple.Create(row, col);
+    }
 }
