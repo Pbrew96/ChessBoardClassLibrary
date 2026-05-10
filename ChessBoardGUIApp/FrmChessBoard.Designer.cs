18fe2e14a00f93be63b0a7cbf3f140a4cab07293
Patrick Brewster <Patrick.shepherd33@gmail.com>
2026-05-10 13:12:49 -0400

    Setup a grid of buttons on the GUI

--- ChessBoardGUIApp/FrmChessBoard.Designer.cs
+++ ChessBoardGUIApp/FrmChessBoard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChessBoardGUIApp
 {
-    partial class Form1
+    partial class FrmChessBoard
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            cmbChessPieces = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            pnlChessBoard = new Panel();
+            SuspendLayout();
+            // 
+            // cmbChessPieces
+            // 
+            cmbChessPieces.FormattingEnabled = true;
+            cmbChessPieces.Items.AddRange(new object[] { "King", "Queen", "Bishop", "Knight", "Rook" });
+            cmbChessPieces.Location = new Point(667, 12);
+            cmbChessPieces.Name = "cmbChessPieces";
+            cmbChessPieces.Size = new Size(121, 23);
+            cmbChessPieces.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(394, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Select a chess piece and its location on the board and see the legal moves";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(615, 15);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Pieces: ";
+            // 
+            // pnlChessBoard
+            // 
+            pnlChessBoard.Location = new Point(3, 30);
+            pnlChessBoard.Name = "pnlChessBoard";
+            pnlChessBoard.Size = new Size(500, 500);
+            pnlChessBoard.TabIndex = 3;
+            // 
+            // FrmChessBoard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(800, 595);
+            Controls.Add(pnlChessBoard);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(cmbChessPieces);
+            Name = "FrmChessBoard";
+            Text = "Chess Board";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox cmbChessPieces;
+        private Label label1;
+        private Label label2;
+        private Panel pnlChessBoard;
     }
 }
--- ChessBoardGUIApp/FrmChessBoard.cs
+++ ChessBoardGUIApp/FrmChessBoard.cs
@@ -1,10 +1,74 @@
+
+/*
+ * Patrick
+ * CST-250
+ * 05/09/2026
+ * Chess Board Project
+ * Activity 2
+ */
+
+using ChessBoardClassLibrary.Models;
+using ChessBoardClassLibrary.Services.BusinessLogicLayer;
+
 namespace ChessBoardGUIApp
 {
-    public partial class Form1 : Form
+    public partial class FrmChessBoard : Form
     {
-        public Form1()
+        // Class level variables
+        private BoardModel _board;
+        private BoardLogic _boardLogic;
+        // 2D array of buttons for the chess board
+        private Button[,] _buttons;
+        public FrmChessBoard()
         {
             InitializeComponent();
+
+            //Initialize the class level variables
+            _board = new BoardModel(8);
+            _boardLogic = new BoardLogic();
+            _buttons = new Button[8, 8];
+
+            // Set up the panel with buttons
+            SetUpButtons();
         }
+        private void SetUpButtons()
+        { 
+            //Declare and initialize
+            //Calculate the size of each button based on
+            // the panel width and the number of buttons needed
+            int buttonSize = pnlChessBoard.Width / _board.Size;
+            //Set the panel to be square
+            pnlChessBoard.Height = pnlChessBoard.Width;
+            // Use nested for loops to loop through the boards Grid
+            for (int row = 0; row < _board.Size; row++)
+            {
+                for (int col = 0; col < _board.Size; col++)
+                {
+                    //Set up each individual button
+                    // Create a new button in the 2D Array
+                    _buttons[row, col] = new Button();
+                    //Get the current button
+                    Button button = _buttons[row, col];
+                    // Set the size of the button
+                    button.Width = buttonSize;
+                    button.Height = buttonSize;
+                    //Set the location of the button
+                    // Using the left and top sides
+                    button.Left = row * buttonSize;
+                    button.Top = col * buttonSize;
+                    //Attach a click event handler to the button
+                    button.Click += BtnSquareClickEH;
+                    //Store the location of the button in
+                    // the tag property using a Point object
+                    button.Tag = new Point(row, col);
+                    //Set the text for the button
+                    button.Text = $"{row}, {col}";
+                    // Add the button to the panels controls
+                    pnlChessBoard.Controls.Add(_buttons[row, col]);
+
+                }
+            }
+
+        } //End of SetUpButtons method
     }
 }
