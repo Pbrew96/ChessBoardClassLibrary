5497fc8813f84808ef2efebacf718ce17ae0b350
Patrick <pbrewskiii@gmail.com>
2026-05-09 23:28:02 -0400

    Added Rook, Bishop, Queen, and King moves

--- ChessBoardClassLibrary/Services/BusinessLogicLayer/BoardLogic.cs
+++ ChessBoardClassLibrary/Services/BusinessLogicLayer/BoardLogic.cs
@@ -1,3 +1,10 @@
+/*
+ * Patrick
+ * CST-250
+ * 05/09/2026
+ * Chess Board Project
+ * Activity 2
+ */
 using ChessBoardClassLibrary.Models;
 
 namespace ChessBoardClassLibrary.Services.BusinessLogicLayer;
@@ -14,7 +21,8 @@ public class BoardLogic
             cell.PieceOccupyingCell = "";
 
         }
-    return board;
+
+        return board;
     }
 
     private bool IsOnBoard(BoardModel board, int row, int col)
@@ -43,12 +51,20 @@ public class BoardLogic
                 board = MarkValidKnightMoves(board, currentCell);
                 break;
             case "rook":
+                board.Grid[currentCell.Row, currentCell.Column].PieceOccupyingCell = "R";
+                board = MarkValidRookMoves(board, currentCell);
                 break;
             case "bishop":
+                board.Grid[currentCell.Row, currentCell.Column].PieceOccupyingCell = "B";
+                board = MarkValidBishopMoves(board, currentCell);
                 break;
             case "queen":
+                board = MarkValidRookMoves(board, currentCell);
+                board = MarkValidBishopMoves(board, currentCell);
                 break;
             case "king":
+                board.Grid[currentCell.Row, currentCell.Column].PieceOccupyingCell = "K";
+                board = MarkValidKingMoves(board, currentCell);
                 break;
             default:
                 //If the piece is not valid, return the board as is
@@ -73,9 +89,80 @@ public class BoardLogic
             if (IsOnBoard(board, currentCell.Row + knightRowMoves[i], currentCell.Column + knightColMoves[i]))
             {
                 //If the cell is on the board, label it as a possible move for the knight
-                board.Grid[currentCell.Row + knightRowMoves[i], currentCell.Column + knightColMoves[i]].IsLegalNextMove = true;
+                board.Grid[currentCell.Row + knightRowMoves[i], currentCell.Column + knightColMoves[i]]
+                    .IsLegalNextMove = true;
             }
-    }
+        }
 
         return board;
+    }
+    private BoardModel MarkValidRookMoves(BoardModel board, CellModel currentCell)
+    {
+        for (int row = 0; row < board.Size; row++)
+        {
+            if (row != currentCell.Row)
+            {
+                board.Grid[row, currentCell.Column].IsLegalNextMove = true;
+            }
+        }
+
+        for (int col = 0; col < board.Size; col++)
+        {
+            if (col != currentCell.Column)
+            {
+                board.Grid[currentCell.Row, col].IsLegalNextMove = true;
+            }
+        }
+
+        return board;
+    }
+    private BoardModel MarkValidBishopMoves(BoardModel board, CellModel currentCell)
+    {
+        int[] rowDirections = { -1, -1, 1, 1 };
+        int[] colDirections = { -1, 1, -1, 1 };
+
+        for (int i = 0; i < rowDirections.Length; i++)
+        {
+            int row = currentCell.Row + rowDirections[i];
+            int col = currentCell.Column + colDirections[i];
+
+            while (IsOnBoard(board, row, col))
+            {
+                board.Grid[row, col].IsLegalNextMove = true;
+
+                row += rowDirections[i];
+                col += colDirections[i];
+            }
+        }
+
+        return board;
+    }
+    private BoardModel MarkValidQueenMoves(BoardModel board, CellModel currentCell)
+    {
+        
+        board = MarkValidRookMoves(board, currentCell);
+        board = MarkValidBishopMoves(board, currentCell);
+
+        return board;
+    }
+    private BoardModel MarkValidKingMoves(BoardModel board, CellModel currentCell)
+    {
+        // Possible king moves
+        int[] rowMoves = { -1, -1, -1, 0, 0, 1, 1, 1 };
+        int[] colMoves = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+        for (int i = 0; i < rowMoves.Length; i++)
+        {
+            int newRow = currentCell.Row + rowMoves[i];
+            int newCol = currentCell.Column + colMoves[i];
+
+            if (IsOnBoard(board, newRow, newCol))
+            {
+                board.Grid[newRow, newCol].IsLegalNextMove = true;
+            }
+        }
+
+        return board;
+    }
+    
 }
